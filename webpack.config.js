91856f8e75c232feb5f63196daa5f047aa47724a
Yoshiaki Togami <tuabobo123@gmail.com>
2020-11-23 17:38:55 +0900

    Move webpack.config to example

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: path.resolve(__dirname, 'src/react-ts-github-calendar.tsx'),
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.tsx', '.css'],
+  },
+};
