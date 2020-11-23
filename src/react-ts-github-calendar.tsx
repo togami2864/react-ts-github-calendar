7c9f2ae6c57a524b814529f3c0d0824780632830
Yoshiaki Togami <tuabobo123@gmail.com>
2020-11-24 04:54:35 +0900

    fixed type for option

--- src/react-ts-github-calendar.tsx
+++ src/react-ts-github-calendar.tsx
@@ -2,18 +2,16 @@ import React, { useRef, useEffect } from 'react';
 import GithubCalendar from 'github-calendar';
 import './style.css';
 
-export type options = {
-  responsive?: boolean;
-  tooltips?: boolean;
-  summary_text?: string;
-  proxy?: (userName: string) => Promise<JSX.Element>;
-  global_stats?: boolean;
-  cache?: number;
-};
-
 export type props = {
   userName: string;
-  option?: options;
+  option: {
+    responsive?: boolean;
+    tooltips?: boolean;
+    summary_text?: string;
+    proxy?: (userName: string) => Promise<JSX.Element>;
+    global_stats?: boolean;
+    cache?: number;
+  };
 };
 
 const ReactGithubCalendar: React.FC<props> = ({ userName, option }) => {
