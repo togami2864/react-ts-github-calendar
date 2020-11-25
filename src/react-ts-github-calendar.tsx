1677efa1b7acdbf7d7d1739e5736411781b1c7af
Yoshiaki Togami <tuabobo123@gmail.com>
2020-11-25 19:48:35 +0900

    fixed type

--- src/react-ts-github-calendar.tsx
+++ src/react-ts-github-calendar.tsx
@@ -4,21 +4,35 @@ import './style.css';
 
 export type props = {
   userName: string;
-  option: {
-    responsive?: boolean;
-    tooltips?: boolean;
-    summary_text?: string;
-    proxy?: (userName: string) => Promise<JSX.Element>;
-    global_stats?: boolean;
-    cache?: number;
-  };
+  responsive?: boolean;
+  tooltips?: boolean;
+  summary_text?: string;
+  proxy?: (userName: string) => Promise<JSX.Element>;
+  global_stats?: boolean;
+  cache?: number;
 };
 
-const ReactGithubCalendar: React.FC<props> = ({ userName, option }) => {
+const ReactGithubCalendar: React.FC<props> = (props) => {
+  const {
+    userName,
+    responsive = false,
+    tooltips = false,
+    summary_text,
+    proxy,
+    global_stats = true,
+    cache,
+  } = props;
   const ref = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
-    GithubCalendar(ref.current, userName, { ...option });
-  }, [option, userName]);
+    GithubCalendar(ref.current, userName, {
+      responsive,
+      tooltips,
+      summary_text,
+      proxy,
+      global_stats,
+      cache,
+    });
+  }, []);
   return <div ref={ref}>now loading</div>;
 };
 
